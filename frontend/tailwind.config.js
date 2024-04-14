34af6809a7a95732a8df010f473b20fd2f01ceaf
Diego Franco <diegotfranco@gmail.com>
2024-04-14 14:56:59 -0400

    atualizando dependencias

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  important: '#root',
+  content: [
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './index.html'
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'Helvetica Neue', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+      },
+      colors: {
+        dark: '#28282B',
+      },
+      fontFamily:{
+        'open-sans': ['Open Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
-}
-
+  // [require("tailwindcss-radix")({
+    // variantPrefix: "rdx"
+  // })],
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
+import { ConfigEnv, defineConfig } from 'vite'
+// import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }: ConfigEnv) => {
+  return {
+    plugins: [
+      react(),
+      // TanStackRouterVite()
+    ],
+    envDir: "./src",
+    server: {
+      port: 3000,
+    },
+    base: "./",
+    resolve: {
+      alias: {
+        "assets": path.resolve(__dirname, "./src/assets"),
+        "components": path.resolve(__dirname, "./src/components"),
+        "modules": path.resolve(__dirname, "./src/modules"),
+        "pages": path.resolve(__dirname, "./src/pages"),
+        "types": path.resolve(__dirname, "./src/types"),
+        "routes": path.resolve(__dirname, "./src/routes"),
+        "utils": path.resolve(__dirname, "./src/utils"),
+        "hooks": path.resolve(__dirname, "./src/hooks"),
+        "contexts": path.resolve(__dirname, "./src/contexts")
+      },
+    },
+  }
+});
